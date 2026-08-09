bd2e264266f6d03ef6386d056e6be415f99e503f
axphxlt <144260748+Nugget1252@users.noreply.github.com>
2026-08-09 17:38:37 +0530

    e

--- test-peer.mjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-fetch('https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js')
-  .then(res => res.text())
-  .then(text => {
-     let i = text.indexOf('does not support WebRTC');
-     console.log(text.substring(i - 200, i + 200));
-  });
--- test-relay.js
+++
@@ -1,7 +0,0 @@
-class PeerJSMockWS {
-  constructor(isHost, roomId, password, nickname) {
-     this.readyState = 1;
-     this.onmessage = null;
-     this.onclose = null;
-  }
-}
