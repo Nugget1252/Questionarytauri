9cea62c19056a6eb9066ef0bcca9a18acd035a36
axphxlt <144260748+Nugget1252@users.noreply.github.com>
2026-08-24 17:38:09 +0530

    ga

--- src/js/app.js
+++ src/js/app.js
@@ -3783,6 +3783,7 @@ if (window._HOT_APP_JS_LOADED && document.currentScript && !document.currentScri
 
     function addPageBookmark(docPath, pageNumber, label = '') {
       if (!pageBookmarks[docPath]) {
+        
         pageBookmarks[docPath] = [];
       }
 
